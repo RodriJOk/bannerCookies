6b5cada88c5aec34109793f47ea6914ee2c25400
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-05 09:35:51 -0300

    Modifications made to the main text, some styles and the configuration of Google Tag Mananger is included.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
 const avisoCookies = document.getElementById('aviso-cookies')
 const fondoAvisoCookies = document.getElementById('fondo-aviso-cookie')
 
+dataLayer = []
+
 if(!localStorage.getItem("cookies-aceptadas")){    
     avisoCookies.classList.add("activo")
     fondoAvisoCookies.classList.add("activo")
+} else{
+    dataLayer.push({'event': 'Cookies acptadas'});
 }
 
 botonAceptarCookies.addEventListener('click', ()=>{
@@ -12,4 +16,6 @@ botonAceptarCookies.addEventListener('click', ()=>{
     fondoAvisoCookies.classList.remove("activo")
 
     localStorage.setItem('cookies-aceptadas', true)
+
+    dataLayer.push({'event': 'Cookies acptadas'});
 })
